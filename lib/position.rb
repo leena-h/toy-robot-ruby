208bdc795dbd9445e8385a648792021f1a2852d0
Leena <leena@hardhat.com.au>
2019-07-16 21:10:41 +1000

    Initialize position

--- lib/position.rb
+++ lib/position.rb
@@ -1,2 +1,10 @@
 class Position
+  attr_reader :x, :y, :direction
+  # VALID_DIRECTIONS = [:north, :east, :west, :south]
+
+  def initialize(x:, y:, direction: :north)
+    @x = x
+    @y = y
+    @direction = direction
+  end
 end
--- test/lib/position_test.rb
+++ test/lib/position_test.rb
@@ -1,4 +1,25 @@
 require 'test_helper'
 
 class PositionTest < Minitest::Test
+  def setup
+    @instance = Position.new(x: 0, y: 0, direction: :north)
+  end
+
+  def test_initializes_with_x_y_and_direction_correctly
+    assert_equal(0, @instance.x)
+    assert_equal(0, @instance.y)
+    assert_equal(:north, @instance.direction)
+  end
+
+  def test_able_to_move_left
+  end
+
+  def test_able_to_move_right
+  end
+
+  def test_able_to_move_back
+  end
+
+  def test_able_to_move_forward
+  end
 end

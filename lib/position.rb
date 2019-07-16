d97b91dfcfb4aacadcf97d1e7243405d347ef801
Leena <leena@hardhat.com.au>
2019-07-16 22:17:33 +1000

    Add ability to move left or right

--- lib/position.rb
+++ lib/position.rb
@@ -1,10 +1,33 @@
 class Position
   attr_reader :x, :y, :direction
-  # VALID_DIRECTIONS = [:north, :east, :west, :south]
+  DIRECTION = {
+    north: 0,
+    east: 1,
+    west: 2,
+    south: 3
+  }.freeze
 
-  def initialize(x:, y:, direction: :north)
+  def initialize(x:, y:, direction: DIRECTION[:north])
     @x = x
     @y = y
     @direction = direction
+  end
+
+  def left
+    @direction =
+      unless @direction == DIRECTION[:north]
+        @direction += 1
+      else
+        DIRECTION[:west]
+      end
+  end
+
+  def right
+    @direction =
+      unless @direction == DIRECTION[:west]
+        @direction += 1
+      else
+        DIRECTION[:north]
+      end
   end
 end
--- test/lib/position_test.rb
+++ test/lib/position_test.rb
@@ -2,19 +2,25 @@ require 'test_helper'
 
 class PositionTest < Minitest::Test
   def setup
-    @instance = Position.new(x: 0, y: 0, direction: :north)
+    @instance = Position.new(x: 0, y: 0, direction: Position::DIRECTION[:north])
   end
 
   def test_initializes_with_x_y_and_direction_correctly
     assert_equal(0, @instance.x)
     assert_equal(0, @instance.y)
-    assert_equal(:north, @instance.direction)
+    assert_equal(Position::DIRECTION[:north], @instance.direction)
   end
 
-  def test_able_to_move_left
+  def test_able_to_move_left_to_change_direction
+    expected = Position::DIRECTION[:west]
+    result = @instance.left
+    assert_equal(expected, result)
   end
 
-  def test_able_to_move_right
+  def test_able_to_move_right_to_change_direction
+    expected = Position::DIRECTION[:east]
+    result = @instance.right
+    assert_equal(expected, result)
   end
 
   def test_able_to_move_back

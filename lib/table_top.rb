25292d642e8d1e0acea90d3971fb66f89f492dd0
Leena <leena@hardhat.com.au>
2019-07-16 23:37:51 +1000

    Add table top tests and implementation

--- lib/table_top.rb
+++ lib/table_top.rb
@@ -1,2 +1,12 @@
 class TableTop
+  attr_reader :columns, :rows
+
+  def initialize(width:, height:)
+    @columns = 0...width
+    @rows = 0...height
+  end
+
+  def within_boundaries?(x_coordinate:, y_coordinate:)
+    @columns.cover?(x_coordinate) && @rows.cover?(y_coordinate)
+  end
 end
--- test/lib/table_top_test.rb
+++ test/lib/table_top_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class TableTopTest < Minitest::Test
+  def setup
+    @instance = TableTop.new(width: 5, height: 5)
+  end
+
+  def test_initialize_with_width_and_height
+    expected_rows = 0...5
+    expected_columns = 0...5
+    assert_equal(expected_rows, @instance.rows)
+    assert_equal(expected_columns, @instance.columns)
+  end
+
+  def test_determines_within_bounds_correctly
+    refute @instance.within_boundaries?(x_coordinate: 6, y_coordinate: 6)
+  end
 end
